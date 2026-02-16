08222759d3215718476491359eb0a265efe55658
Fábio Batista <fabio@atelie.dev.br>
2026-02-16 17:08:28 -0300

    Adds vscodium to the GUI tools

--- shared/gui-tools.nix
+++ shared/gui-tools.nix
@@ -16,6 +16,7 @@
     jetbrains-toolbox # Jetbrains Toolbox
     gimp3-with-plugins # GNU Image Manipulation Program
     mission-center # Monitor your CPU, Memory, Disk, Network and GPU usage
+    vscodium # Open source source code editor developed by Microsoft (VS Code without MS branding/telemetry/licensing)
 
     # LibreOffice and OnlyOffice
     onlyoffice-desktopeditors
