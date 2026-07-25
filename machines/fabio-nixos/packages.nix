06c1a57b9491c6fba29f7dbe0fc28228b8c2cc3b
Fábio Batista <fabio@atelie.dev.br>
2026-07-25 19:19:56 -0300

    Remove Thunderbird

--- machines/fabio-nixos/packages.nix
+++ machines/fabio-nixos/packages.nix
@@ -32,7 +32,7 @@
     enableVirtualCamera = true;
   };
 
-  programs.thunderbird.enable = true;
+  # programs.thunderbird.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
