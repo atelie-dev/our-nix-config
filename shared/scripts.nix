8cf2b403462ae9ce152bbce4c532538064b17d9d
Fábio Batista <fabio@atelie.dev.br>
2026-08-17 19:11:43 -0300

    Removes `nix flake update` since we no longer stage the lockfile

--- shared/scripts.nix
+++ shared/scripts.nix
@@ -12,7 +12,6 @@
       text = ''
         set -exo pipefail
         cd /etc/nixos
-        nix flake update
         ${pkgs.nixos-rebuild-ng}/bin/nixos-rebuild switch --flake ".#$(hostname)" -v --log-format internal-json "$@" |& ${pkgs.nix-output-monitor}/bin/nom --json
       '';
     })
